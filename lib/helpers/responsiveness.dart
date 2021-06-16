c508f84ca61eba3c049e3cf295f2e2ded2535a96
santos-enoque <santosenoque.ss@gmail.com>
2021-06-16 14:54:31 +0200

    responsiveness

---
+++ lib/helpers/responsiveness.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+const int largeScreenSize = 1366;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+const int customScreenSize = 1100;
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget largeScreen;
+  final Widget mediumScreen;
+  final Widget smallScreen;
+
+  const ResponsiveWidget(
+      {Key key,
+      @required this.largeScreen,
+      this.mediumScreen,
+      this.smallScreen,
+      })
+      : super(key: key);
+
+static bool isSmallScreen(BuildContext context) =>
+MediaQuery.of(context).size.width < smallScreenSize;
+
+static bool isMediumScreen(BuildContext context) =>
+MediaQuery.of(context).size.width >= mediumScreenSize &&
+MediaQuery.of(context).size.width < largeScreenSize;
+
+static bool isLargeScreen(BuildContext context) =>
+MediaQuery.of(context).size.width >= largeScreenSize;
+
+static bool isCustomSize(BuildContext context) =>
+MediaQuery.of(context).size.width >= mediumScreenSize &&
+MediaQuery.of(context).size.width <= customScreenSize;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints){
+      double _width = constraints.maxWidth;
+      if(_width >= largeScreenSize){
+        return largeScreen;
+      }
+      else if(_width < largeScreenSize && _width >= mediumScreenSize){
+        return mediumScreen ?? largeScreen;
+      }
+      else{
+        return smallScreen ?? largeScreen;
+      }
+    });
+  }
+}
--- lib/layout.dart
+++ lib/layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_dashboard/helpers/responsiveness.dart';
 import 'package:flutter_web_dashboard/widgets/large_screen.dart';
 import 'package:flutter_web_dashboard/widgets/small_screen.dart';
 
@@ -12,7 +13,7 @@ class SiteLayout extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: SmallScreen(),
+      body: ResponsiveWidget(largeScreen: LargeScreen(), smallScreen: SmallScreen(),),
       
     );
   }
